2db417cde508869bf1911c668b911522ce2f7354
Jason Perkins <jasontperkins@gmail.com>
2013-04-15 06:56:53 -0400

    Fixing how many items per page in paginate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
+	WillPaginate.per_page = 10
 
 	#Returns the full title on a per-page basis.
 	def full_title(page_title)
-		base_title = "Ruby on Rails Tutorial Sample App"
+		base_title = "The Blog of the Future"
 		if page_title.empty?
 			base_title
 		else
