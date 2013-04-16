efb435cb704395b392080e64f60cf732205d0038
Jason Perkins <jasontperkins@gmail.com>
2013-04-16 01:52:24 -0400

    Fixed something

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130415221230) do
-
-  create_table "hash_tags", force: true do |t|
-    t.integer  "tag_id"
-    t.integer  "micropost_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "hash_tags", ["micropost_id"], name: "index_hash_tags_on_micropost_id"
-  add_index "hash_tags", ["tag_id"], name: "index_hash_tags_on_tag_id"
+ActiveRecord::Schema.define(version: 20130412144227) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -42,12 +32,6 @@ ActiveRecord::Schema.define(version: 20130415221230) do
   add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
   add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
-
-  create_table "tags", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "name"
